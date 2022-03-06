83b6963293eabeb7a658c664c20838777ccf460f
nirban256 <nirban256@gmail.com>
2022-03-06 23:14:28 +0530

    added the frontend part of minting website

---
+++ minting website/main.js
@@ -0,0 +1,52 @@
+// connect to Moralis server
+const serverUrl = "https://pj6gy5sxjwhn.usemoralis.com:2053/server";
+const appId = "cV4cFcdFGnbanFPOV1Z8cLJRBSDw1pCWEH9pGMIJ";
+Moralis.start({ serverUrl, appId });
+
+// add Metamask
+async function login() {
+    let user = Moralis.User.current();
+    if (!user) {
+        user = await Moralis.authenticate();
+    }
+    console.log("logged in user:", user);
+}
+document.getElementById("btn-login").onclick = login;
+
+async function logOut() {
+    await Moralis.User.logOut();
+    console.log("logged out");
+}
+document.getElementById("btn-logout").onclick = logOut;
+
+
+// increase and decrease function
+const value = document.getElementById('value');
+const price = document.getElementById('price');
+let p = 1.1;
+
+document.getElementById('increase').addEventListener("click", (e) => {
+    e.preventDefault();
+    if (parseInt(value.innerHTML) < 5) {
+        let num = parseInt(value.innerHTML);
+        value.innerHTML = num + 1;
+        price.innerHTML = p * value.innerHTML;
+    }
+});
+
+document.getElementById('decrease').addEventListener("click", (e) => {
+    e.preventDefault();
+    if (parseInt(value.innerHTML) > 1) {
+        let num = parseInt(value.innerHTML);
+        value.innerHTML = num - 1;
+        price.innerHTML = p * value.innerHTML;
+    }
+});
+
+// menu mobile display
+const menu = document.getElementById('mobile-menu');
+const navUl = document.getElementById('login');
+
+menu.addEventListener('click', () => {
+    navUl.classList.toggle('show');
+});
