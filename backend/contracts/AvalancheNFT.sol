44ee62b4f8faaf479168dde28d3fad03b1168177
nirban256 <nirban256@gmail.com>
2022-03-19 12:06:27 +0530

    changed contract name

---
+++ backend/contracts/AvalancheNFT.sol
@@ -0,0 +1,77 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity >=0.8.0 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract AvalancheNFT is ERC721URIStorage, Ownable {
+    using Counters for Counters.counter;
+
+    Counters.Counter private _tokenIds;
+
+    uint256 public mintPrice;
+    uint256 public totalSupply;
+    uint256 public maxSupply;
+    uint256 public maxPerWallet;
+    bool public isPublicMintEnabled;
+    string internal baseTokenUri;
+    address payable public withdrawWallet;
+    mapping(address => uint256) public walletMints;
+
+    constructor() payable ERC721("Avax Wolverine", "AW") {
+        mintPrice = 0.75 ether;
+        totalSupply = 0;
+        maxSupply = 10;
+        maxPerWallet = 3;
+        withdrawWallet = 0xE00C58c5337Dc82C631c32e4B2C9739E3c221635;
+    }
+
+    function setIsPublicMintEnabled(bool _isPublicMintEnabled)
+        external
+        onlyOwner
+    {
+        isPublicMintEnabled = _isPublicMintEnabled;
+    }
+
+    function setBaseTokenUri(string calldata _baseTokenUri) external onlyOwner {
+        baseTokenUri = _baseTokenUri;
+    }
+
+    function tokenURI(uint256 _tokenId)
+        public
+        view
+        override
+        returns (string memory)
+    {
+        require(_exists(_tokenId), "Token does not exists");
+        return
+            string(
+                abi.encodePacked(
+                    baseTokenUri,
+                    Strings.toString(_tokenId),
+                    ".json"
+                )
+            );
+    }
+
+    function withdraw() external onlyOwner {
+        (bool success, ) = withdrawWallet.call{value: address(this).balance}(
+            ""
+        );
+        require(success, "withdraw failed");
+    }
+
+    function mintNFT(address recipient, string memory tokenURI)
+        public
+        onlyOwner
+        returns (uint256)
+    {
+        _tokenIds.increment();
+        uint256 newTokenId = _tokenIds.current();
+        _mint(recipient, newTokenId);
+        _setTokenURI(newTokenId, tokenURI);
+        return newTokenId;
+    }
+}
